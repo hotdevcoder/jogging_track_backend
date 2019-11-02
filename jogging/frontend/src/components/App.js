dc361fd4eb872485edf727eb4332727045dc727c
audricus19 <audricus2019@gmail.com>
2019-11-03 00:27:47 +1000

    add redux and redux-thunk

--- jogging/frontend/src/components/App.js
+++ jogging/frontend/src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from '../store';
 
 class App extends Component {
   render() {
-    return <h1>React App</h1>;
+    return (
+      <Provider store={store}>
+        <h1>React App</h1>
+      </Provider>
+    )
   }
 }
 
